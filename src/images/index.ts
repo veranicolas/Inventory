38be78c6b21d3626941cb56ec84d963dcf5197f9
Nicolas Vera <nicolasveraa97@gmail.com>
2023-11-21 00:21:50 -0300

    added: content to drawer, avatar and logout button

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,3 +1,4 @@
 export const LOGIN_IMAGE = require('./login.jpg')
 export const LOGIN_SVG = require('./login_image.svg')
 export const HAMBURGUER_ICON = require('./hamburguer.png')
+export const AVATAR = require('./avatar.jpg')
--- src/routes/MainNavigator.tsx
+++ src/routes/MainNavigator.tsx
@@ -1,7 +1,10 @@
 import { createStackNavigator } from "@react-navigation/stack";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { DrawerContentScrollView, DrawerItem, DrawerItemList, createDrawerNavigator } from "@react-navigation/drawer";
 import { Home, Login } from "../views";
 import { Inventory } from "../views/Inventory";
+import { Image, Text, View } from "react-native";
+
+import { AVATAR } from "../images";
 
 const Stack = createStackNavigator()
 const Drawer = createDrawerNavigator()
@@ -9,7 +12,27 @@ const Drawer = createDrawerNavigator()
 const HomeNavigator = () =>{
 
     return(
-        <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Navigator initialRouteName="Home" drawerContent={props => {
+            return (
+              <DrawerContentScrollView contentContainerStyle={{display:'flex', flex:1, flexDirection:'column', justifyContent:'flex-start'}} {...props}>
+
+                {/* picture and name */}
+                <View style={{paddingVertical:25, display:'flex', justifyContent:'center', flexDirection:'row'}}>
+                    <Image source={AVATAR} style={{width:140, height:140, borderRadius:100}} resizeMode="contain" />
+                </View>
+                <Text style={{fontSize:25, textAlign:'center', paddingVertical:10, marginBottom:15}}>Nicolas</Text>
+
+                {/* list with routes */}
+                <DrawerItemList {...props} />
+
+                {/* logout button */}
+                <View style={{flex:1, display:'flex', justifyContent:'flex-end'}}>
+                    <DrawerItem style={{backgroundColor:'red'}} label="LOGOUT" labelStyle={{color:'white', textAlign:'center', width:'100%'}} onPress={() => props.navigation.navigate("Login")} />
+                </View>
+                
+              </DrawerContentScrollView>
+            )
+          }}>
             <Drawer.Screen name="Home" component={Home} options={{headerShown:false}} />
             <Drawer.Screen name="Inventario" component={Inventory} options={{headerShown:false}} />
         </Drawer.Navigator>
